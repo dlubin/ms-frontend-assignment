f1f24124a9e49ec06feea2fb58486b5990c02f73
David Lubin <david@davidlubindigital.com>
2023-08-15 20:01:42 -0400

    First commit. Just making sure everything works

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const css = require('./scss/main.scss');
-
+const cardsData = require('./static/data/cards-data.json').default; // Added by David
 
 /**
  *
@@ -22,3 +22,16 @@ const createImageFinderMethod = ()=> {
 }
 
 window._getThumbnailURL = createImageFinderMethod();
+
+// David's code begins
+// Retrieve users data
+async function getUsers() {
+  const response = await fetch(cardsData);
+  return response.json();
+}
+
+window.onload = () => {
+  getUsers().then((usersData) => {
+    console.log(usersData);
+  });
+}
