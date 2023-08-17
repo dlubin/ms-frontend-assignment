c0a2d476cffe35031e5cbc29d648b439ba2a0f5a
David Lubin <david@davidlubindigital.com>
2023-08-16 21:40:04 -0400

    Logic and markup done. Styles mostly done but need to tweak dimensions, particularly for smaller screens

--- src/index.js
+++ src/index.js
@@ -34,6 +34,31 @@ window.onload = () => {
   getUsers().then((usersData) => {
     var users = usersData.users;
     users = users.sort((a,b) => a.teamName.localeCompare(b.teamName) || a.lastName.localeCompare(b.lastName))
-    console.log(users)
+    const profileGrid = document.getElementById('profile-card-grid');
+    const profileTemplate = document.getElementById('profile-card');
+    for (let profile of users) {
+      createCard(profileGrid, profileTemplate, profile);
+    }
   });
+}
+
+/* Duplicate template card, plug in profile data, and append to grid */
+function createCard(profileGrid, profileTemplate, profile) {
+  const clone = profileTemplate.content.cloneNode(true);
+  let thumbnail = clone.querySelectorAll('.thumbnail');
+  thumbnail[0].src = _getThumbnailURL(profile.thumbnail);
+  setTextField(clone, '.icaName', profile.icaName);
+  setTextField(clone, '.title', profile.title);
+  setTextField(clone, '.profile-bioShort', profile.bioShort);
+  setTextField(clone, '.linkedInSkill', `${profile.linkedInSkill},`);
+  setTextField(clone, '.teamName', `${profile.teamName},`);
+  let email = clone.querySelectorAll('.email');
+  email[0].href = `mailto:${profile.email}`;
+  profileGrid.appendChild(clone);
+}
+
+/* Set the text value of an element */
+function setTextField(parentEl, selector, value) {
+  let el = parentEl.querySelectorAll(selector);
+  el[0].textContent = value;
 }
